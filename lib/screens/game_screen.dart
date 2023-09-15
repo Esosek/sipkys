2c06405bab7bbd3a8aea1f5bc5b422c508f5848f
Ales Zima <ales@cpex.cz>
2023-09-15 20:55:55 +0200

    ScoreProvider

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GameScreen extends StatelessWidget {
+import 'package:sipkys/providers/game_mode_provider.dart';
+import 'package:sipkys/providers/players_provider.dart';
+
+class GameScreen extends ConsumerWidget {
   const GameScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final players = ref.read(activePlayersProvider);
+    final gameMode = ref.read(gameModeProvider);
+
+    String playerText = 'hráči';
+    if (players.length == 1) {
+      playerText = 'hráč';
+    } else if (players.length > 4) {
+      playerText = 'hráčů';
+    }
+
+    final screenTitle =
+        'Mód $gameMode - ${players.length.toString()} $playerText';
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        title: Text(
+          screenTitle,
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onPrimaryContainer,
+              ),
+        ),
+      ),
+      body: ,
+    );
   }
 }
